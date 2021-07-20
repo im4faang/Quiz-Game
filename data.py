44b926a6461a3f2bd3131da84576c96873458f9e
im4faang <86999433+im4faang@users.noreply.github.com>
2021-07-20 11:39:47 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,46 @@
+question_data = [
+    {
+        "question": "Clefairy was intended to be Ash&#039;s starting Pok&eacute;"
+                    "mon in the pilot episode of the cartoon.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;"
+                    "JavaScript&quot;.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "&quot;Half-Life 2&quot; runs on the Source Engine.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "The 2016 United States Presidential Election is the first time "
+                    "Hillary Clinton has run for President.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "Solid Snake is actually a clone from the DNA of Big Boss "
+                    "in the Metal Gear Solid series&#039; history.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "In the game Dead by Daylight, the killer Michael Myers is refered to as &quot;The Shape&quot;.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "Daft Punk originated in France.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "In Pok&eacute;mon, Arbok evolves into Seviper.",
+        "correct_answer": "False",
+    }
+]
